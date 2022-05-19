c51b2a1f1dd5a41d11370ac21005bd098a5383c4
plutoin <uleage@gmail.com>
2022-05-19 17:49:45 +0900

    05.19

---
+++ JavaScript/057.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// str = 'hello strict';
+
+// var undefined = 'undefined';
+// var Infinity = 100;
+// Math.PI = 0;
+
+// const Employee = {
+//     firstname: 'John',
+//     lastname: 'Doe'
+// }
+
+// console.log(Employee.firstname);
+
+// delete Employee.firstname;
+
+// console.log(Employee.firstname);
+
+// delete Object.prototype;
+
+function myFunc(a, a, b) {
+  console.log(a + a + b);
+}
+
+myFunc(1, 2, 3);
